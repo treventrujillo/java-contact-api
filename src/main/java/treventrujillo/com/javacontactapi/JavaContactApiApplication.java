2e83750455922717d064138b58a61babf9ba1880
Treven Trujillo <treventrujillo@gmail.com>
2018-02-07 17:34:26 -0700

    init commit

---
+++ src/main/java/treventrujillo/com/javacontactapi/JavaContactApiApplication.java
@@ -0,0 +1,12 @@
+package treventrujillo.com.javacontactapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaContactApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaContactApiApplication.class, args);
+	}
+}
---
+++ src/test/java/treventrujillo/gmail/com/javacontactapi/JavaContactApiApplicationTests.java
@@ -0,0 +1,16 @@
+package treventrujillo.gmail.com.javacontactapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaContactApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
